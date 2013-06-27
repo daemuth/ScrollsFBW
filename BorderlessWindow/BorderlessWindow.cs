a1eee874aaf96873c6a83f11c35a7a50630e663c
levela <1204939@live.abertay.ac.uk>
2013-06-27 13:38:48 +0200

    Possible fix for Multiple screens issue

--- BorderlessWindow/BorderlessWindow.cs
+++ BorderlessWindow/BorderlessWindow.cs
@@ -5,14 +5,17 @@ using System.Collections;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Windows.Interop;
+using System.Drawing;   
 using UnityEngine;
+using System.Windows;
+using System.Windows.Forms;
 
 
 namespace BorderlessWindow
 {
     public class BorderlessWindow : BaseMod
     {
-        public Rect screenPosition;
+        //public Rect screenPosition;
 
         [DllImport("user32.dll")]
         static extern IntPtr SetWindowLong(IntPtr hwnd, int _nIndex, int dwNewLong);
@@ -20,13 +23,11 @@ namespace BorderlessWindow
         static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
         [DllImport("user32.dll", EntryPoint = "FindWindow")]
         private static extern IntPtr FindWindow(string lp1, string lp2);
-
-
+    
         const uint SWP_SHOWWINDOW = 0x0040;
         const int GWL_STYLE = -16;
         const int WS_BORDER = 1;
 
-        //initialize everything here, Game is loaded at this point
         public BorderlessWindow()
         {
         }
@@ -63,14 +64,16 @@ namespace BorderlessWindow
         public override bool BeforeInvoke(InvocationInfo info, out object returnValue)
         {
             returnValue = null;
-            int unityScreenWidth = UnityEngine.Screen.currentResolution.width;
-            int unityScreenHeight = UnityEngine.Screen.currentResolution.height;
+
+            int unityScreenWidth = UnityEngine.Screen.width;
+            int unityScreenHeight = UnityEngine.Screen.height;
+
+           // int asd =  System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
 
             IntPtr handle = FindWindow(null, "Scrolls");
 
             SetWindowLong(handle, GWL_STYLE, WS_BORDER);
             SetWindowPos(handle, 0, 0, 0, unityScreenWidth, unityScreenHeight, SWP_SHOWWINDOW);
-
 
             return false;
         }
